e65343c62acd19eeb34f28078ca072004fb7e0b5
Andrew McDonald <andrewmcdonald27@gmail.com>
2022-01-13 00:14:03 -0500

    update log freq

--- train.py
+++ train.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 
     # wandb logging
     wandb_logger = pl.loggers.WandbLogger(project="comet-flows")
-    wandb_logger.watch(model, log="all")
+    wandb_logger.watch(model, log="all", log_freq=10)
 
     trainer = pl.Trainer.from_argparse_args(args)
     trainer.logger = wandb_logger
